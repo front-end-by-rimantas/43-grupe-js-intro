342373531b0c6ea7b368a87612e1ac78373fd86f
Rimantas <belauzas@yahoo.com>
2023-03-30 10:29:44 +0300

    String

---
+++ js/02-strings.js
@@ -0,0 +1,14 @@
+console.clear();
+
+/*
+STRINGS - simboliu sarasas/junginys, tekstas -> paragrafai -> sakiniu -> zodziu
+*/
+
+console.log('Labas');
+console.log('rytas');
+console.log('Labas rytas');
+
+console.log("Labas");
+console.log("rytas");
+console.log("Labas rytas");
+
