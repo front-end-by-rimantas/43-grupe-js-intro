d04a897c0ca84c954e0bd5c9c43c90cd3de9a369
Rimantas <belauzas@yahoo.com>
2023-03-30 09:43:08 +0300

    Console.log()

---
+++ index.js
@@ -0,0 +1,21 @@
+// Vienos eilutes komentaras
+
+// Multi
+// line
+// komentaras
+
+/*
+Multi
+line
+komentaras
+*/
+
+console.log(42);    // Komentaras salia
+console.log(41);
+console.log(40);
+
+// extra
+
+console.log(39);
+console.log(38);
+console.log(37);
