8760310b907dffbebd64a6f4d2d52ede7da2deaf
Rimantas <belauzas@yahoo.com>
2023-04-06 13:45:57 +0300

    Intro into FOR loop

--- js/09-for.js
+++ js/09-for.js
@@ -3,24 +3,25 @@ console.clear();
 FOR - loop, ciklas
 */
 
-const marks = [10];
+const marks = [10, 2, 8, 4, 6];
 const count = marks.length;
 
 let sum = 0;
+let index = 0;
 
-sum = sum + marks[0];
+sum += marks[index++];
 console.log('Suma:', sum);
 
-sum = sum + marks[1];
+sum += marks[index++];
 console.log('Suma:', sum);
 
-sum = sum + marks[2];
+sum += marks[index++];
 console.log('Suma:', sum);
 
-sum = sum + marks[3];
+sum += marks[index++];
 console.log('Suma:', sum);
 
-sum = sum + marks[4];
+sum += marks[index++];
 console.log('Suma:', sum);
 
 
