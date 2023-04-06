4305294252c5d7139f909c10c23ccd130ceace08
Rimantas <belauzas@yahoo.com>
2023-04-06 13:33:11 +0300

    Matematiniai ir priskirimo operatoriai

---
+++ js/09-for.js
@@ -0,0 +1,28 @@
+console.clear();
+/*
+FOR - loop, ciklas
+*/
+
+const marks = [10];
+const count = marks.length;
+
+let sum = 0;
+
+sum = sum + marks[0];
+console.log('Suma:', sum);
+
+sum = sum + marks[1];
+console.log('Suma:', sum);
+
+sum = sum + marks[2];
+console.log('Suma:', sum);
+
+sum = sum + marks[3];
+console.log('Suma:', sum);
+
+sum = sum + marks[4];
+console.log('Suma:', sum);
+
+
+const average = sum / count;
+console.log('Vidurkis:', average);
