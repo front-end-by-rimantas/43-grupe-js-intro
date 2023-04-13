bca667cbfe035ba0046505e064a04c1162c3e734
Rimantas <belauzas@yahoo.com>
2023-04-13 14:56:07 +0300

    Loops

--- js/18-loops.js
+++ js/18-loops.js
@@ -114,6 +114,85 @@ console.log(marks2xMap);
 
 // FILTER - palieka tik tai ko nori
 const studentMarks = [10, 2, -7, 8, -5, 4, 6];
-const filteredMarks = studentMarks.filter(m => m >= 1 && m <= 10);
+const filteredMarks = studentMarks
+    .filter(mark => mark >= 1 && mark <= 10);
 console.log(studentMarks);
 console.log(filteredMarks);
+
+// REDUCE - subendrinti rezultata pagal pateikta logika
+const reducedSum = marks
+    .reduce((sum, mark) => sum + mark);
+console.log('reduce:', reducedSum);
+
+const reducedSum2 = marks
+    .reduce((sum, mark) => sum + mark, 0);
+console.log('reduce2:', reducedSum2);
+
+
+const reduce1 = [1, 2, 3, 4].reduce((t, n) => t + n);
+// 1+2+3+4 = 10
+console.log('1:', reduce1);
+
+const reduce2 = [1, 2, 3, 4].reduce((t, n) => t + n, 0);
+// 0+1+2+3+4 = 10
+console.log('2:', reduce2);
+
+const reduce3 = [1, 2, 3, 4].reduce((t, n) => t + n, 100);
+// 100+1+2+3+4 = 110
+console.log('3:', reduce3);
+
+const reduce4 = [1, 2, 3, 4].reduce((t, n) => t + n, -100);
+// -100+1+2+3+4 = -90
+console.log('4:', reduce4);
+
+const reduce5 = [1, 2, 3, 4].reduce((t, n) => t - n);
+// 1-2-3-4 = -8
+console.log('5:', reduce5);
+
+const reduce6 = [1, 2, 3, 4].reduce((t, n) => t - n, 0);
+// 0-1-2-3-4 = -10
+console.log('6:', reduce6);
+
+const reduce7 = [1, 2, 3, 4].reduce((t, n) => t - n, 100);
+// 100-1-2-3-4 = 90
+console.log('7:', reduce7);
+
+const reduce8 = [1, 2, 3, 4].reduce((t, n) => t - n, -100);
+// -100-1-2-3-4 = -110
+console.log('8:', reduce8);
+
+const reduce9 = [1, 2, 3, 4].reduce((t, n) => t * n, 10);
+console.log('9:', reduce9);
+
+const reduce10 = [1, 2, 3, 4].reduce((t, n) => t / n, 10);
+console.log('10:', reduce10);
+
+const reduce11 = [1, 2, 3, 4].reduce((t, n) => t % n, 10);
+console.log('11:', reduce11);
+
+// SORT
+marks.sort();
+console.log(marks);
+
+marks.sort((a, b) => a - b);
+console.log(marks);
+
+marks.sort((a, b) => b - a);
+console.log(marks);
+
+console.clear();
+
+// VIDURKIS
+const pazymiai = [10, 2, 'Petras', true, 8, -7, [], {}, undefined, NaN, Infinity, -Infinity, 4, 6, Math.E, Math.PI, 3.14, null, 0];
+
+const tikriPazymiai = pazymiai
+    .filter(p => typeof p === 'number')
+    .filter(p => p > 0)
+    .filter(p => p <= 10)
+    .filter(p => p % 1 === 0);
+
+const vidurkis = tikriPazymiai
+    .reduce((s, p) => s + p, 0) / tikriPazymiai.length;
+
+console.log(tikriPazymiai);
+console.log(vidurkis);
